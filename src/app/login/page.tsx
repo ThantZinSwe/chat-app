f82bfe8ed87f860ba1b6017962a38783d01029a2
thantzinswe <thantzinswe01@gmail.com>
2023-07-22 20:43:13 +0630

    login,register and chat design

---
+++ src/app/login/page.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-24 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="flex items-center space-x-3">
+          <Image
+            src="/chat-app-logo.jpeg"
+            alt="My Chat Logo"
+            width={120}
+            height={120}
+            className="mx-auto rounded-full object-center object-fill"
+          />
+        </div>
+        <h2 className="text-2xl font-bold leading-9 text-gray-900">Welcome!</h2>
+        <p className="text-gray-900 leading-3">Sign in to your account.</p>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-semibold text-primary-600 hover:text-primary-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Doesn't have an account?
+          <Link
+            href="/register"
+            className="font-semibold leading-6 text-primary-600 hover:text-primary-500"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        raleway: ["Raleway"],
+      },
+      colors: {
+        "primary-50": "#eef2ff",
+        "primary-100": "#e0e7ff",
+        "primary-200": "#c7d2fe",
+        "primary-300": "#a5b4fc",
+        "primary-400": "#818cf8",
+        "primary-500": "#6366f1",
+        "primary-600": "#4f46e5",
+        "primary-700": "#4338ca",
+        "primary-800": "#3730a3",
+        "primary-900": "#312e81",
+        "primary-950": "#1e1b4b",
       },
     },
   },
   plugins: [],
-}
+};
